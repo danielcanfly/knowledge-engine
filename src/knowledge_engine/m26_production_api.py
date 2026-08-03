b805d0579af508d93f1e0ced24e96e8841a89a52
huaihsuanbusiness <38602668+huaihsuanbusiness@users.noreply.github.com>
2026-08-03 18:14:19 +0800

    feat: route production API through semantic closure v3

--- src/knowledge_engine/m26_production_api.py
+++ src/knowledge_engine/m26_production_api.py
@@ -6,7 +6,7 @@ from typing import Any
 
 from . import m26_ask_api
 from . import m26_pa7_arbitrary_query_runtime as legacy_runtime
-from .m26_aq_semantic_runtime_patch import install as install_aq_semantic_runtime_patch
+from .m26_aq_semantic_runtime_patch_v3 import install as install_aq_semantic_runtime_patch
 from .m26_intent_compat import classify_with_semantic_compat
 from .m26_pa7_semantic_closure_runtime import run_owner_arbitrary_query
 
