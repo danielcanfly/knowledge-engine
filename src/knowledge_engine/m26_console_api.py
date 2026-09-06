a2e90a1628e67dc5b3328c76ea7b3f5735e5e810
danielcanfly <daniel.huang.tw.409@gmail.com>
2026-09-07 00:56:55 +0700

    feat(qa): install answer quality inbox in console app

--- src/knowledge_engine/m26_console_api.py
+++ src/knowledge_engine/m26_console_api.py
@@ -6,12 +6,12 @@ from .m26_admin_corpus import install_admin_corpus
 from .m26_admin_health import install_admin_health
 from .m26_admin_ingestion import install_admin_ingestion_routes
 from .m26_admin_overview import install_admin_overview
-from .m26_admin_qa import install_admin_qa
 from .m26_admin_settings import install_admin_settings
 from .m26_admin_usage import install_admin_usage
 from .m26_console_p05_ask_playground import router as playground_router
 from .m26_golden_questions_admin import install_golden_questions_admin
 from .m26_jobs_rollback_api import install_jobs_rollback_routes
+from .m26_qa_inbox_integration import install_qa_inbox
 from .m26_suggested_questions_admin import install_suggested_questions_admin
 from .m26_translation_gateway_public_api import create_app as create_public_app
 
@@ -22,7 +22,7 @@ def create_app():
     install_admin_overview(app)
     install_admin_ingestion_routes(app, include_job_reads=False)
     install_admin_corpus(app)
-    install_admin_qa(app)
+    install_qa_inbox(app)
     app.include_router(playground_router())
     install_suggested_questions_admin(app)
     install_admin_usage(app)
